d05ec389134e634bdb862d77e07d8bf38d6e5ef7
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-05-10 11:30:42 +0300

    Update main.script.js

--- main.script.js
+++ main.script.js
@@ -17,7 +17,7 @@ let carLvl2 = document.querySelector(".carsLvl2")
 let carLvl3 = document.querySelector(".carsLvl3")
 
 let lvl = 1;
-console.dir(cars)
+
 btnLvl1.onclick = function (){
     btnLvl1.classList.add("isActive")
     btnLvl2.classList.remove("isActive")
@@ -87,6 +87,7 @@ if (lvl == 1) {
     carLvl3.innerHTML += html;
   }
 }
+
 carLvl1.onclick = function(event){
     let element = event.target;
     let parentElement = element.parentElement;
